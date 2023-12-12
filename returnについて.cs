d4311f6dd79983078f9abf8b3c67e96d5e438ff4
forkgrip <hakusuroku@gmail.com>
2023-12-12 17:16:53 +0900

    クラス復習

---
+++ returnについて.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Globalization;
+using System.Xml.XPath;
+
+namespace return101
+{
+    public class Hemo
+    {
+
+        public void number(int a)
+        {
+            int aXb = a * 60; 
+            
+        }
+
+        static void Main()
+        {
+    
+        int result = number(70);
+
+        Console.WriteLine(result);
+
+        }
+
+
+    }
+}
