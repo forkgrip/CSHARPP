9b8151ea1969a451fcebec679df35eceadbd23d8
forkgrip <hakusuroku@gmail.com>
2023-12-19 16:33:15 +0900

    引数の復習

---
+++ 引数/H1k1.cs
@@ -0,0 +1,25 @@
+using System;
+
+
+namespace hiki101
+{
+    class Program
+    {
+        public int nnnnm;
+
+
+        static void Main(string[] args)
+        {
+            Method();
+        }
+
+        static public void Method()
+        {
+
+            Console.WriteLine("num=" + num);
+
+        }
+
+
+    }
+}
