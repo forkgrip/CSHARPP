1cdb965525df32a02e30b6efc8f2a39bb8262481
forkgrip <hakusuroku@gmail.com>
2024-01-17 18:44:06 +0900

    再度引数理解ll

--- content/引数/Retrun3.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace first
-{
-    class Program2
-    {
-        
-    }
-}
--- content/引数/Return2.cs
+++ content/引数/Return2.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Text;//UTF8使うため
 
+
 namespace first
 {
-    class Program
+    static class Program
     {
         static int money = 300;
 
@@ -15,39 +16,66 @@ namespace first
             //売っているものを入力
             string drink1 = "水";
             string drink2 = "茶";
+            string Saving = "テキスト保存機";
+            //この関数作る
+            string ShowSave = "保存テキスト閲覧";
             string exit = "出てく";   
             //ユーザー入力
-            Console.WriteLine("所持金" + money + "円");
-            Console.WriteLine("水(100円)か茶(200円)か(出てく)か");
-            string choseDrink = Console.ReadLine();
-            //whileは、nullのときに、もう一回選択できるようにする（continueにより）
+
+
+            //選択を繰り返す動作にする。while(ture)
             while(true)
                 {
+                    Console.WriteLine("所持金" + money + "円");
+                    Console.WriteLine("水(100円)か茶(200円)か(出てく)か(テキスト保存機)");
+                    string ChoseTask = Console.ReadLine();
                     //水が選ばれたとき
-                    if (drink1 == choseDrink)
+                    if (ChoseTask == drink1)
                     {
-                    int usedmoney =VendingMachine(1, ref money);
-                    break;
+                        int usedmoney =VendingMachine(1, ref money);
+                        ChoseTask = "null";
                     }
                     //茶が選ばれたとき
-                    else if (drink2 == choseDrink)
+                    else if (ChoseTask == drink2)
                     {
-                    int usedmoney = VendingMachine(2, ref money);
-                    Console.WriteLine(money + "円残っています");
-                    break;
+                        int usedmoney = VendingMachine(2, ref money);
+                        Console.WriteLine(money + "円残っています");
+                        ChoseTask = "null";
                     }
+                    //テキスト保存機が選ばれたととき
+                    else if (ChoseTask == Saving)
+                    {
+                        //文字列を読み取り、SentenceSaveメソッドでその文字列を送り、そのメソッドで文字列を保保存する
+                        Console.WriteLine("では、テキストを入力してください");
+                        string InputText = Console.ReadLine();
+                        string savedtext = SentenceSave(ref InputText);
+                        Console.WriteLine("文字列「" + savedtext + "」を保存しました。");
+                    }
+                    /*テキスト保存したものを見れるようにする
+                    else if (ChoseTask == ShowSave)
+                    {
+                        
+                    }
+                    */
                     //break処理で選ぶループを抜ける
-                    else if (exit == choseDrink)
+                    else if (ChoseTask == exit)
                     {
-                    Console.WriteLine("自販機から離れました");
+                    Console.WriteLine("自販機？から離れました");
                     break;
                     }
+
                     //なんにも入力されてないとき、continueで、もっかいループの最初に行く
-                    else if (choseDrink == null)
+                    else if (ChoseTask == null)
                     {
-                    Console.WriteLine("水か茶を入力してください。");
+                    Console.WriteLine("括弧内を入力してください。");
                     continue;
-                    } 
+                    }
+                    //ユーザーが選択したものをnullにするので、exitをしない限り、この選択画面を実行し続ける→これないと、一回選択したあと、無限ループになる。
+                    if (ChoseTask != null)
+                    {
+                        ChoseTask = null;
+                    }
+ 
                 }
             Console.WriteLine("残りの所持金: " + money + "円");
         }
@@ -78,6 +106,13 @@ namespace first
                 Console.WriteLine("所持金が足りません");
                 return useMoney;
             }
+        }
+
+        //テキストを保存するメソッド(渡された引数を保存するだけ)
+        static string SentenceSave (ref String KeepSentence)
+        {
+            string SaveText1 = KeepSentence;
+            return KeepSentence;
         }
     }
 }
